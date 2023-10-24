8c2ee390994e0c161e713f5ae0f441e3d863e7af
ibtissam <ibtissamelhani36@gmail.com>
2023-10-24 18:38:54 +0200

    FAQ

--- file.js
+++ file.js
@@ -1,6 +1,28 @@
+// humberger-menu
 const menuH = document.querySelector(".fa-solid");
 const navbar = document.querySelector("nav");
 
-menuH.addEventListener('click', ()=>{
-    navbar.classList.toggle('mobile-menu');
-})
+menuH.addEventListener("click", () => {
+  navbar.classList.toggle("mobile-menu");
+});
+
+// FAQ
+const faqs = document.querySelectorAll(".bloc");
+faqs.forEach((bloc) => {
+  bloc.addEventListener("click", () => {
+    let isIn = false;
+
+    if (bloc.classList.contains("active")) {
+      isIn = true;
+    }
+    for (faq of faqs) {
+      faq.classList.remove("active");
+    }
+
+    bloc.classList.toggle("active");
+
+    if (isIn) {
+      bloc.classList.remove("active");
+    }
+  });
+});
