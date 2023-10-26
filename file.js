81cef5a6053e938998e0525230cb24f1769464d4
ibtissam <ibtissamelhani36@gmail.com>
2023-10-26 09:40:51 +0200

    validation de formulaire login

--- file.js
+++ file.js
@@ -27,18 +27,43 @@ faqs.forEach((bloc) => {
   });
 });
 
-// slider page about
+// slider des image
 const next = document.querySelector(".fa-circle-chevron-right");
 const prec = document.querySelector(".fa-circle-chevron-left");
 
-next.addEventListener("click", () => {
-  const widthSlide = document.querySelector(".equipe").offsetWidth;
-  const items = document.querySelector(".team");
-  items.scrollLeft += widthSlide;
-});
+// next.addEventListener("click", () => {
+//   const widthSlide = document.querySelector(".equipe").offsetWidth;
+//   const items = document.querySelector(".team");
+//   items.scrollLeft += widthSlide;
+// });
 
-prec.addEventListener("click", () => {
-  const widthSlide = document.querySelector(".equipe").offsetWidth;
-  const items = document.querySelector(".team");
-  items.scrollLeft -= widthSlide;
+// prec.addEventListener("click", () => {
+//   const widthSlide = document.querySelector(".equipe").offsetWidth;
+//   const items = document.querySelector(".team");
+//   items.scrollLeft -= widthSlide;
+// });
+
+// validation formulaire login
+
+const logForm = document.getElementById("login-form");
+const logMail = document.getElementById("login-mail");
+const password = document.getElementById("password");
+
+// console.log(logForm);
+// console.log(logMail);
+// console.log(password);
+
+
+logForm.addEventListener("submit", function (e) {
+  
+  const emailError = document.querySelector(".email-error");
+  const passwordError = document.querySelector(".password-error");
+  if (logMail.value.length == 0) {
+    e.preventDefault();
+    emailError.style.display = "block";
+  }
+  if (password.value.length < 8) {
+    e.preventDefault();
+    passwordError.style.display = "block";
+  }
 });
